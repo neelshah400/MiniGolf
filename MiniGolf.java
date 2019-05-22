b4dbc150330b3fae4b9a311f8808034e5b43c7dd
Neel Shah <10014194@sbschools.org>
2019-05-22 09:28:58 -0400

    Add files

---
+++ MiniGolf.java
@@ -0,0 +1,33 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.*;
+import javafx.scene.media.AudioClip;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
+import javafx.event.*;
+import javafx.scene.input.*;
+import javafx.scene.text.*;
+
+public class MiniGolf extends Application{
+
+	public static void main(String[]args){
+	
+		launch();
+	
+	}
+	
+	public void start(Stage stage){
+	
+	
+	
+	}
+
+}
