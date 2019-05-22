dfef2ce92ed14bbe33ac92aa6d4c8870b4a7e05a
Neel Shah <10014194@sbschools.org>
2019-05-22 09:40:26 -0400

    Part 1

--- MiniGolf.java
+++ MiniGolf.java
@@ -19,15 +19,36 @@ import javafx.scene.text.*;
 public class MiniGolf extends Application{
 
 	public static void main(String[]args){
-	
+
 		launch();
-	
+
 	}
-	
+
 	public void start(Stage stage){
-	
-	
-	
+
+		stage.setTitle("MiniGolf");
+		Group root = new Group();
+		Canvas canvas = new Canvas(800, 400); // change canvas size
+		root.getChildren().add(canvas);
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		Image trooper = new Image("trooper.jpg");
+		gc.drawImage(trooper, 180, 100);
+
+		stage.show();
+
+	}
+
+	public class AnimateObjects extends AnimationTimer{
+
+		public void handle(long now){
+
+			//
+
+		}
+
 	}
 
 }
